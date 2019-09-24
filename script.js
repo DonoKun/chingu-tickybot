faf443eaebc0cc921544747cbb6290f2df4b8d0c
Donovan Gomez <donovanthedev@gmail.com>
2019-09-23 23:47:08 -0700

    add fade in effects

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// fade in
+window.onload = function() {
+    document.querySelector('#fade').style.opacity = 1;
+}
+
+window.onscroll = function() {
+    document.querySelector('.how-it-works-wrapper').style.opacity = 1;
+}
